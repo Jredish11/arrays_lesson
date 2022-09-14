d97b2f717bfe6f79ea54ba20bf1bb964f8b26196
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-09-14 11:24:13 -0600

    Add method of choice

--- arrays.rb
+++ arrays.rb
@@ -10,7 +10,10 @@ students.pop
 ages.shift
 #This line of code calls to bring an element to the front of an array. also can undue .shift. Nothing should happen to this variable bc I didn't specified which element to move and there has been no .shift method used before hand.
 balances.unshift
-#This line of using .lenght method will show a number value of how many elements are in the tuition_paid array.
+#This line of using .length method will show a number value of how many elements are in the tuition_paid array.
 tuition_paid.length
 
 #Index poistions refer to the position of an element in a given array.  For example, the index position for "Dave" in the <students> array would be 2.
+
+#This line of code would have a similar outcome to .length.  By using .count method it returns the number of elements in the array. Might be easier to remember than .length.
+tuition_paid.count
